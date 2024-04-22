12da908aba4013f01c1677f7869e875047550079
devmosis <ryzond6728@gmail.com>
2024-04-23 02:45:08 +0900

    axelar 에러 하드코딩 수정

--- packages/axelar/src/codegen/axelar/permission/v1beta1/service.registry.ts
+++ packages/axelar/src/codegen/axelar/permission/v1beta1/service.registry.ts
@@ -1,6 +1,6 @@
 import { GeneratedType, Registry } from "@cosmjs/proto-signing";
-import { RegisterControllerRequest, DeregisterControllerRequest, UpdateGovernanceKeyRequest } from "./tx";
-export const registry: ReadonlyArray<[string, GeneratedType]> = [["/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest", axelar.permission.v1beta1.RegisterControllerRequest], ["/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest", axelar.permission.v1beta1.DeregisterControllerRequest], ["/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest", axelar.permission.v1beta1.UpdateGovernanceKeyRequest]];
+import { RegisterControllerRequest, DeregisterControllerRequest, UpdateGovernanceKeyRequest, } from "./tx";
+export const registry: ReadonlyArray<[string, GeneratedType]> = [["/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest", RegisterControllerRequest], ["/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest", DeregisterControllerRequest], ["/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest", UpdateGovernanceKeyRequest]];
 export const load = (protoRegistry: Registry) => {
   registry.forEach(([typeUrl, mod]) => {
     protoRegistry.register(typeUrl, mod);
@@ -8,41 +8,41 @@ export const load = (protoRegistry: Registry) => {
 };
 export const MessageComposer = {
   encoded: {
-    registerController(value: axelar.permission.v1beta1.RegisterControllerRequest) {
+    registerController(value: RegisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest",
-        value: axelar.permission.v1beta1.RegisterControllerRequest.encode(value).finish()
+        typeUrl: "/axelar.permission.v1beta1.RegisterControllerRequest",
+        value: RegisterControllerRequest.encode(value).finish()
       };
     },
-    deregisterController(value: axelar.permission.v1beta1.DeregisterControllerRequest) {
+    deregisterController(value: DeregisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest",
-        value: axelar.permission.v1beta1.DeregisterControllerRequest.encode(value).finish()
+        typeUrl: "/axelar.permission.v1beta1.DeregisterControllerRequest",
+        value: DeregisterControllerRequest.encode(value).finish()
       };
     },
-    updateGovernanceKey(value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest) {
+    updateGovernanceKey(value: UpdateGovernanceKeyRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
-        value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest.encode(value).finish()
+        typeUrl: "/axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
+        value: UpdateGovernanceKeyRequest.encode(value).finish()
       };
     }
   },
   withTypeUrl: {
-    registerController(value: axelar.permission.v1beta1.RegisterControllerRequest) {
+    registerController(value: RegisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest",
+        typeUrl: "/axelar.permission.v1beta1.RegisterControllerRequest",
         value
       };
     },
-    deregisterController(value: axelar.permission.v1beta1.DeregisterControllerRequest) {
+    deregisterController(value: DeregisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest",
+        typeUrl: "/axelar.permission.v1beta1.DeregisterControllerRequest",
         value
       };
     },
-    updateGovernanceKey(value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest) {
+    updateGovernanceKey(value: UpdateGovernanceKeyRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
+        typeUrl: "/axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
         value
       };
     }
@@ -50,40 +50,40 @@ export const MessageComposer = {
   fromJSON: {
     registerController(value: any) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest",
-        value: axelar.permission.v1beta1.RegisterControllerRequest.fromJSON(value)
+        typeUrl: "/axelar.permission.v1beta1.RegisterControllerRequest",
+        value: RegisterControllerRequest.fromJSON(value)
       };
     },
     deregisterController(value: any) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest",
-        value: axelar.permission.v1beta1.DeregisterControllerRequest.fromJSON(value)
+        typeUrl: "/axelar.permission.v1beta1.DeregisterControllerRequest",
+        value: DeregisterControllerRequest.fromJSON(value)
       };
     },
     updateGovernanceKey(value: any) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
-        value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest.fromJSON(value)
+        typeUrl: "/axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
+        value: UpdateGovernanceKeyRequest.fromJSON(value)
       };
     }
   },
   fromPartial: {
-    registerController(value: axelar.permission.v1beta1.RegisterControllerRequest) {
+    registerController(value: RegisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.RegisterControllerRequest",
-        value: axelar.permission.v1beta1.RegisterControllerRequest.fromPartial(value)
+        typeUrl: "/axelar.permission.v1beta1.RegisterControllerRequest",
+        value: RegisterControllerRequest.fromPartial(value)
       };
     },
-    deregisterController(value: axelar.permission.v1beta1.DeregisterControllerRequest) {
+    deregisterController(value: DeregisterControllerRequest) {
       return {
-        typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.DeregisterControllerRequest",
-        value: axelar.permission.v1beta1.DeregisterControllerRequest.fromPartial(value)
+        typeUrl: "/axelar.permission.v1beta1.DeregisterControllerRequest",
+        value: DeregisterControllerRequest.fromPartial(value)
       };
     },
-    updateGovernanceKey(value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest) {
+    updateGovernanceKey(value: UpdateGovernanceKeyRequest) {
       return {
         typeUrl: "/axelar.permission.v1beta1.axelar.permission.v1beta1.UpdateGovernanceKeyRequest",
-        value: axelar.permission.v1beta1.UpdateGovernanceKeyRequest.fromPartial(value)
+        value: UpdateGovernanceKeyRequest.fromPartial(value)
       };
     }
   }
